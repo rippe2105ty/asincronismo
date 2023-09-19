0bf610b4ee588aa757542bd57bf2756a3d4df041
rippe2105 <rippe2105@gmail.com>
2023-09-19 18:51:17 -0500

    1 tarea

--- src/app.js
+++ src/app.js
@@ -1,3 +1,10 @@
-function getRandomNumber() {}
+function getRandomNumber() {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const randomNumber = Math.floor(Math.random() * 11); // Genera un número entre 0 y 10
+        resolve(randomNumber); // Resuelve la promesa con el número aleatorio
+      }, 3000); // Espera 3 segundos antes de resolver la promesa
+    });
+  }
 
-module.exports = getRandomNumber;
+  module.exports = getRandomNumber;
